5f7076e45cb96081c51c9e5f77d0261e39b98b0a
Aravind Mangadu Chakkaravarthy <aravind@aravinds-mbp.lan>
2020-08-04 17:52:57 -0400

    Preparation

---
+++ algorithms/src/main/java/leetcode/array/easy/MajorityElement.java
@@ -0,0 +1,55 @@
+package leetcode.array.easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+ *
+ * You may assume that the array is non-empty and the majority element always exist in the array.
+ *
+ * Example 1:
+ *
+ * Input: [3,2,3]
+ * Output: 3
+ * Example 2:
+ *
+ * Input: [2,2,1,1,1,2,2]
+ * Output: 2
+ */
+public class MajorityElement {
+
+    public static int majorityElement(int[] nums) {
+        //TODO Implement own sorting algorithms for better understanding of various sorting algorithms.
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+
+    }
+
+    public static int majorityElementBySort(int[] nums){
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                int count = map.get(nums[i]);
+                map.put(nums[i], count+1);
+            }else{
+                map.put(nums[i],1);
+            }
+        }
+        for(int j=0;j<nums.length;j++){
+            if(map.get(nums[j]) > (nums.length/2)){
+                return nums[j];
+            }
+        }
+        throw new IllegalArgumentException("Should not reach here");
+    }
+
+    public static void main(String args[]){
+        int[] nums = {2,2,1,1,1,2,2};
+        System.out.println(majorityElement(nums));
+        System.out.println(majorityElementBySort(nums));
+    }
+
+
+}
