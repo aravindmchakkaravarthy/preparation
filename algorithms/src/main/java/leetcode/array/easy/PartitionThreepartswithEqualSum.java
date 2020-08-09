9d6ce9184907a25637db7ca898b7c4f6b3d83203
Aravind Mangadu Chakkaravarthy <aravind@aravinds-mbp.lan>
2020-08-09 13:06:25 -0400

    Adding solution for partition array into three parts with equal sum

---
+++ algorithms/src/main/java/leetcode/array/easy/PartitionThreepartswithEqualSum.java
@@ -0,0 +1,48 @@
+package leetcode.array.easy;
+
+/**
+ * Given an array A of integers, return true if and only if we can partition the array into three non-empty parts with equal sums.
+ *
+ * Formally, we can partition the array if we can find indexes i+1 < j with (A[0] + A[1] + ... + A[i] == A[i+1] + A[i+2] + ... + A[j-1] == A[j] + A[j-1] + ... + A[A.length - 1])
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: A = [0,2,1,-6,6,-7,9,1,2,0,1]
+ * Output: true
+ * Explanation: 0 + 2 + 1 = -6 + 6 - 7 + 9 + 1 = 2 + 0 + 1
+ * Example 2:
+ *
+ * Input: A = [0,2,1,-6,6,7,9,-1,2,0,1]
+ * Output: false
+ * Example 3:
+ *
+ * Input: A = [3,3,6,5,-2,2,5,1,-9,4]
+ * Output: true
+ * Explanation: 3 + 3 = 6 = 5 - 2 + 2 + 5 + 1 - 9 + 4
+ */
+public class PartitionThreepartswithEqualSum {
+
+    public static boolean canThreePartsEqualSum(int[] A) {
+        int sum = 0;
+        for(int x:A) sum +=x;
+        int partSum = sum / 3;
+        int currentSum =0;
+        int partitions = 0;
+        for(int x:A){
+            currentSum += x;
+            if(currentSum == partSum){
+                currentSum = 0;
+                partitions++;
+            }
+        }
+        return currentSum == 0 && partitions >= 3;
+    }
+
+    public static void main(String[] args){
+        int[] nums = {0,2,1,-6,6,-7,9,1,2,0,1};
+        System.out.println(canThreePartsEqualSum(nums));
+
+    }
+}
