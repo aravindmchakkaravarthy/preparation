f871f60c1d122d589ed37e55db6ac0c0a10aea7e
Aravind Mangadu Chakkaravarthy <aravind@aravinds-mbp.lan>
2020-08-08 21:31:11 -0400

    Adding solution for Maximum Distance to closest

---
+++ algorithms/src/main/java/leetcode/array/easy/MaxiumumDistanceToClosest.java
@@ -0,0 +1,57 @@
+package leetcode.array.easy;
+/**
+ * In a row of seats, 1 represents a person sitting in that seat, and 0 represents that the seat is empty.
+ *
+ * There is at least one empty seat, and at least one person sitting.
+ *
+ * Alex wants to sit in the seat such that the distance between him and the closest person to him is maximized.
+ *
+ * Return that maximum distance to closest person.
+ *
+ * Example 1:
+ *
+ * Input: [1,0,0,0,1,0,1]
+ * Output: 2
+ * Explanation:
+ * If Alex sits in the second open seat (seats[2]), then the closest person has distance 2.
+ * If Alex sits in any other open seat, the closest person has distance 1.
+ * Thus, the maximum distance to the closest person is 2.
+ * Example 2:
+ *
+ * Input: [1,0,0,0]
+ * Output: 3
+ * Explanation:
+ * If Alex sits in the last seat, the closest person is 3 seats away.
+ * This is the maximum distance possible, so the answer is 3.
+ */
+public class MaxiumumDistanceToClosest {
+
+    public static int maxDistToClosest(int[] seats) {
+        int lp = 0, fp = 0, maxFp = 0, maxLp = 0;
+        int runningCount = 0;
+        for (int i=0;i<seats.length;i++){
+            if(seats[i] == 0){
+                lp = i;
+                if(runningCount == 0){
+                    lp=i;
+                    fp=i;
+                }
+                runningCount++;
+                if((lp-fp) >= (maxLp - maxFp)){
+                    maxLp = lp;
+                    maxFp = fp;
+                }
+            }else{
+                runningCount = 0;
+            }
+        }
+        if(maxLp == seats.length-1) return maxLp;
+        return (maxFp+maxLp)/2;
+
+    }
+
+    public static void main(String[] args){
+        int[] nums = {1,0,0,0};
+        System.out.println(maxDistToClosest(nums));
+    }
+}
