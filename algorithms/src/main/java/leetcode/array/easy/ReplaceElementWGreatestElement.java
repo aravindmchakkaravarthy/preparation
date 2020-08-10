03e35c9b3e93056a904487b56ea5fce94c437423
Aravind Mangadu Chakkaravarthy <aravind@aravinds-mbp.lan>
2020-08-10 15:35:46 -0400

    Added solution for Relacing elements with greatest element on the right side

---
+++ algorithms/src/main/java/leetcode/array/easy/ReplaceElementWGreatestElement.java
@@ -0,0 +1,22 @@
+package leetcode.array.easy;
+
+public class ReplaceElementWGreatestElement {
+
+    public static int[] replaceElements(int[] arr) {
+        int max = -1;
+        for(int i=arr.length-1;i>=0;i--){
+            int temp = arr[i];
+            arr[i] = max;
+            if(temp > max){
+                max=temp;
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args){
+        int[] nums = {17,18,5,4,6,1};
+        replaceElements(nums);
+        PrintUtil.print(nums);
+    }
+}
