4c5f91aa383c6e91424dbc93fca1706a2da95459
Aravind Mangadu Chakkaravarthy <aravind@aravinds-mbp.lan>
2020-08-11 21:12:27 -0400

    Added solution for can make arithmetic progression from a sequence

---
+++ algorithms/src/main/java/leetcode/array/easy/CanMakeArithmeticProgressionFromSequence.java
@@ -0,0 +1,45 @@
+package leetcode.array.easy;
+
+import java.util.Arrays;
+
+/**
+ * Given an array of numbers arr. A sequence of numbers is called an arithmetic progression if the difference between any two consecutive elements is the same.
+ *
+ * Return true if the array can be rearranged to form an arithmetic progression, otherwise, return false.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: arr = [3,5,1]
+ * Output: true
+ * Explanation: We can reorder the elements as [1,3,5] or [5,3,1] with differences 2 and -2 respectively, between each consecutive elements.
+ * Example 2:
+ *
+ * Input: arr = [1,2,4]
+ * Output: false
+ * Explanation: There is no way to reorder the elements to obtain an arithmetic progression.
+ *
+ *
+ * Constraints:
+ *
+ * 2 <= arr.length <= 1000
+ * -10^6 <= arr[i] <= 10^6
+ */
+public class CanMakeArithmeticProgressionFromSequence {
+
+    public static boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);
+        int diff = Math.abs(arr[0] - arr[1]);
+        for(int i=2;i<arr.length;i++){
+            int thisDiff = Math.abs(arr[i] - arr[i-1]);
+            if(thisDiff != diff) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        int[] nums = {3,5,1};
+        System.out.println(canMakeArithmeticProgression(nums));
+    }
+}
