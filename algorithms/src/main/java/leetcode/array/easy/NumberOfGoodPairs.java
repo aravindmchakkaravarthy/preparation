fe3b336669e41ec1476665ee28788df309ab79fb
Aravind Mangadu Chakkaravarthy <aravind@aravinds-mbp.lan>
2020-08-11 21:26:03 -0400

    Added solution for number of good pairs

---
+++ algorithms/src/main/java/leetcode/array/easy/NumberOfGoodPairs.java
@@ -0,0 +1,54 @@
+package leetcode.array.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array of integers nums.
+ *
+ * A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+ *
+ * Return the number of good pairs.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,2,3,1,1,3]
+ * Output: 4
+ * Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+ * Example 2:
+ *
+ * Input: nums = [1,1,1,1]
+ * Output: 6
+ * Explanation: Each pair in the array are good.
+ * Example 3:
+ *
+ * Input: nums = [1,2,3]
+ * Output: 0
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 100
+ * 1 <= nums[i] <= 100
+ */
+public class NumberOfGoodPairs {
+
+    public static int numIdenticalPairs(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        for(int x:nums){
+            if(map.containsKey(x)){
+                count += map.get(x);
+            }
+            map.put(x, map.getOrDefault(x,0)+1);
+        }
+        return count;
+    }
+
+    public static void main(String[] args){
+        int[] nums = {1,2,3,1,1,3};
+        System.out.println(numIdenticalPairs(nums));
+    }
+}
